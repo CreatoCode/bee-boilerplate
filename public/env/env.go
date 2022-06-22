ca1dfe79a1f0ed5502cf4795c27db8d02b42bd33
steven.lin <linjinxing493@pingan.com.cn>
2022-06-22 19:48:10 +0800

    implement a better interface, haha

--- public/env/env.go
+++ public/env/env.go
@@ -10,12 +10,11 @@ import (
 
 type env struct {
 	syncMap sync.Map
-	IEnv
 }
 
-var g_env = &env{}
-
 var g_logger = logger.New(logger.Env)
+
+var g_env = &env{}
 
 func init() {
 	for _, e := range os.Environ() {
@@ -31,84 +30,6 @@ func init() {
 func SharedInstance() IEnv {
 	return g_env
 }
-
-// func getGOENV(string, error) {
-// 	if file := os.Getenv("GOENV"); file != "" {
-// 		if file == "off" {
-// 			return "", fmt.Errorf("GOENV=off")
-// 		}
-// 		return file, nil
-// 	}
-// 	dir, err := os.UserConfigDir()
-// 	if err != nil {
-// 		return "", err
-// 	}
-// 	if dir == "" {
-// 		return "", fmt.Errorf("missing user-config dir")
-// 	}
-// 	file, err := filepath.Join(dir, "go", "env")
-// 	if err != nil {
-// 		return "", err
-// 	}
-// 	if file == "" {
-// 		return "", fmt.Errorf("missing runtime env file")
-// 	}
-
-// 	var runtimeEnv string
-// 	data, err := ioutil.ReadFile(file)
-// 	if err != nil {
-// 		return "", err
-// 	}
-// 	envStrings := strings.Split(string(data), "\n")
-// 	for _, envItem := range envStrings {
-// 		envItem = strings.TrimSuffix(envItem, "\r")
-// 		envKeyValue := strings.Split(envItem, "=")
-// 		if len(envKeyValue) == 2 && strings.TrimSpace(envKeyValue[0]) == key {
-// 			runtimeEnv = strings.TrimSpace(envKeyValue[1])
-// 		}
-// 	}
-// 	return runtimeEnv, nil
-// }
-
-// func GetGOBIN() string {
-// 	// The one set by user explicitly by `export GOBIN=/path` or `env GOBIN=/path command`
-// 	gobin := strings.TrimSpace(get("GOBIN", ""))
-// 	if gobin == "" {
-// 		var err error
-// 		// The one set by user by running `go env -w GOBIN=/path`
-// 		gobin, err = GetRuntimeEnv("GOBIN")
-// 		if err != nil {
-// 			// The default one that Golang uses
-// 			return filepath.Join(build.Default.GOPATH, "bin")
-// 		}
-// 		if gobin == "" {
-// 			return filepath.Join(build.Default.GOPATH, "bin")
-// 		}
-// 		return gobin
-// 	}
-// 	return gobin
-// }
-
-// // GetGOPATH returns GOPATH environment variable as a string.
-// // It will NOT be an empty string.
-// func GetGOPATH() string {
-// 	// The one set by user explicitly by `export GOPATH=/path` or `env GOPATH=/path command`
-// 	gopath := strings.TrimSpace(get("GOPATH", ""))
-// 	if gopath == "" {
-// 		var err error
-// 		// The one set by user by running `go env -w GOPATH=/path`
-// 		gopath, err = GetRuntimeEnv("GOPATH")
-// 		if err != nil {
-// 			// The default one that Golang uses
-// 			return build.Default.GOPATH
-// 		}
-// 		if gopath == "" {
-// 			return build.Default.GOPATH
-// 		}
-// 		return gopath
-// 	}
-// 	return gopath
-// }
 
 func get[T int | string](key string, defaultValue T) T {
 	if val, ok := g_env.syncMap.Load(key); ok {
